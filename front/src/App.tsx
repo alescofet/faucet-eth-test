b82eaa6199f601ff24c36737b3bffc4537a10c90
Alex Escofet <alex.escofet96@gmail.com>
2025-04-12 11:42:40 +0200

    changed routing to RouterProvider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Link, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Balance from './components/Balance';
 import Faucet from './components/Faucet';
@@ -6,38 +6,52 @@ import Home from './components/Home';
 import { Toaster } from 'sonner';
 import Transfer from './components/Transfer';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/faucet", element: <Faucet /> },
+      { path: "/transfer", element: <Transfer /> },
+      { path: "/balance", element: <Balance /> },
+    ],
+  },
+]);
+
+// Root component with the layout
+function Root() {
+  return (
+    <div className="min-h-screen bg-background">
+      <nav className="p-4 bg-primary">
+        <div className="container flex gap-4 mx-auto">
+          <Link to="/" className="text-white hover:text-gray-200">
+            Inicio
+          </Link>
+          <Link to="/faucet" className="text-white hover:text-gray-200">
+            Faucet
+          </Link>
+          <Link to="/transfer" className="text-white hover:text-gray-200">
+            Transfer
+          </Link>
+          <Link to="/balance" className="text-white hover:text-gray-200">
+            Balance
+          </Link>
+        </div>
+      </nav>
+      <main className="container mx-auto mt-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-background">
-        <nav className="p-4 bg-primary">
-          <div className="container flex gap-4 mx-auto">
-            <Link to="/" className="text-white hover:text-gray-200">
-              Inicio
-            </Link>
-            <Link to="/faucet" className="text-white hover:text-gray-200">
-              Faucet
-            </Link>
-            <Link to="/transfer" className="text-white hover:text-gray-200">
-              Transfer
-            </Link>
-            <Link to="/balance" className="text-white hover:text-gray-200">
-              Balance
-            </Link>
-          </div>
-        </nav>
-
-        <main className="container mx-auto mt-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/faucet" element={<Faucet />} />
-            <Route path="/transfer" element={<Transfer />} />
-            <Route path="/balance" element={<Balance />} />
-          </Routes>
-        </main>
-      </div>
+    <>
+      <RouterProvider router={router} />
       <Toaster richColors position="bottom-center" />
-    </Router>
+    </>
   );
 }
 
